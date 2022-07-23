4f14e8cf8dff8db9ab0d56973d32ed5d1eb84acb
Alan <alan.fer09@gmail.com>
2022-07-22 22:44:00 -0300

    add experience view

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import "./App.css";
-import AboutMe from "./components/AboutMe/AboutMe";
-
+import Experience from "./components/Experience/Experience";
 function App() {
   return (
     <div>
-      <div className="App">
-        <nav className="navbar">
-          <button className="options">Mi experiencia </button>
-          <button className="options">Mis proyectos</button>
-          <button className="options">Acerca de mi</button>
-        </nav>
-        <div className="content_wrap">
-          <div className="title_container">
-            <h1 className="principal_title">ALAN FERNÁNDEZ</h1>
-            <h2>WEB DEVELOPER</h2>
-          </div>
-          <h3>2 años de experiencia en desarrollo web</h3>
+      <img src={process.env.PUBLIC_URL + "/principal_background.jpg"} />
+      {/*
+      <nav className="navbar">
+        <button className="options">Mi experiencia </button>
+        <button className="options">Mis proyectos</button>
+        <button className="options">Acerca de mi</button>
+      </nav>
+      <div className="content_wrap">
+        <div className="title_container">
+          <h1 className="principal_title">ALAN FERNÁNDEZ</h1>
+          <h2>WEB DEVELOPER</h2>
         </div>
+        <h3>2 años de experiencia en desarrollo web</h3>
       </div>
-      <AboutMe />
+      */}
+
+      <Experience />
     </div>
   );
 }
---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,48 @@
+import "./Experience.css";
+export default function Experience() {
+  return (
+    <section>
+      <header>Mi experiencia</header>
+      <p>
+        Trabajé un año en el equipo de frontend de Hello Auto, empresa de
+        tecnología dedicada a pólizas de seguros de autos. Mis tareas dentro de
+        la empresa consistía en colaborar con otros desarrolladores para
+        automatizar los procesos de creación de pólizas de seguro de auto a
+        través de una app web, trabajando con toda la información que el usuario
+        aportaba a la plataforma. Trabajé principalmente con React y Material
+        UI. Nos organizábamos a través de la metodología ágil SCRUM
+      </p>
+      <h2>conocimientos</h2>
+      <div>
+        <span>FRONTEND</span>
+        <ul>
+          <li>JavaScript</li>
+          <li>HTML5</li>
+          <li>css3</li>
+          <li>React</li>
+          <li>Redux</li>
+          <li>Next</li>
+        </ul>
+      </div>
+      <div>
+        <span>BACKEND</span>
+        <ul>
+          <li>Node</li>
+          <li>Express</li>
+          <li>PostgreSQL</li>
+          <li>Mongodb</li>
+          <li>Sequelize</li>
+        </ul>
+      </div>
+      <div>
+        <span>OTRO</span>
+        <ul>
+          <li>GIT</li>
+          <li>figma</li>
+          <li>Babel</li>
+          <li>Webpack</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
